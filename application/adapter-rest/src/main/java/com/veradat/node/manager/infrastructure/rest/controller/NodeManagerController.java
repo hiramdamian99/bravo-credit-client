70cf650e6366b4490bbf33b0b7251916d0b6ff79
Hiram Damian <hiramdamian99@gmail.com>
2024-11-13 17:37:49 -0600

    Updated NodeManagerController.java

--- application/adapter-rest/src/main/java/com/veradat/node/manager/infrastructure/rest/controller/NodeManagerController.java
+++ application/adapter-rest/src/main/java/com/veradat/node/manager/infrastructure/rest/controller/NodeManagerController.java
@@ -16,45 +16,46 @@ import com.veradat.nodemanager.domain.model.KeyResponseDTO;
 import com.veradat.nodemanager.domain.model.Mapping;
 import com.veradat.nodemanager.domain.model.NodeMapping;
 import com.veradat.nodemanager.domain.model.NodeMappingCommand;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/** The type Product controller. */
+/**
+ * The type Product controller.
+ */
 @RestController
-public class NodeManagerController implements NodeManagerApi {
+public class NodeManagerController implements NodeManagerApi
+{
 
     static {
-        IdentifierManager.registerClassIdentifier(NodeManagerController.class, "NMC");
+        IdentifierManager.registerClassIdentifier(NodeManagerController.class,"NMC");
     }
 
     private final Logger logger = LoggerFactory.getLogger(NodeManagerController.class);
     private final NodeMappingAsyncInputPort nodeMappingAsyncInputPort;
 
     @Autowired
-    public NodeManagerController(NodeMappingAsyncInputPort nodeMappingAsyncInputPort) {
+    public NodeManagerController(NodeMappingAsyncInputPort nodeMappingAsyncInputPort){
         this.nodeMappingAsyncInputPort = nodeMappingAsyncInputPort;
     }
 
-    public ResponseEntity<List<NodeMapping>> postNodeMappingRequest(NodeMappingCommand command) {
-        IdentifierManager.registerMethodIdentifier("postNodeMappingRequest", "PNMR");
+    public ResponseEntity<List<NodeMapping>> postNodeMappingRequest(@RequestBody NodeMappingCommand command) {
+        IdentifierManager.registerMethodIdentifier("postNodeMappingRequest","PNMR");
         logger.info("postNodeMappingRequest");
-        List<NodeMapping> mappings =
-                nodeMappingAsyncInputPort.createNodeMapping(
-                        command.getOriginNodeId(), command.getProcessId(), command.getNodes());
+        List<NodeMapping> mappings = nodeMappingAsyncInputPort.createNodeMapping(command.getOriginNodeId(), command.getProcessId(), command.getNodes());
         HttpStatus status = mappings.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK;
         return new ResponseEntity<>(mappings, status);
     }
 
     @Override
-    public ResponseEntity<NodeMapping> getNode(String originNode) {
-        IdentifierManager.registerMethodIdentifier("getNode", "GN");
+    public ResponseEntity<NodeMapping> getNode(@RequestBody String originNode) {
+        IdentifierManager.registerMethodIdentifier("getNode","GN");
 
         logger.info("getNode");
         NodeMapping node = nodeMappingAsyncInputPort.getNode(originNode);
@@ -62,8 +63,8 @@ public class NodeManagerController implements NodeManagerApi {
         return new ResponseEntity<>(node, HttpStatus.OK);
     }
 
-    public ResponseEntity<Mapping> getProcess(String enqueryNodeId) {
-        IdentifierManager.registerMethodIdentifier("getProcess", "GP");
+    public ResponseEntity<Mapping> getProcess(@RequestBody String enqueryNodeId) {
+        IdentifierManager.registerMethodIdentifier("getProcess","GP");
 
         logger.info("getProcess");
         Mapping nodeMapping = nodeMappingAsyncInputPort.getProcessId(enqueryNodeId);
@@ -74,18 +75,10 @@ public class NodeManagerController implements NodeManagerApi {
     }
 
     @Override
-    public ResponseEntity<KeyResponseDTO> getPublicKey(
-            String nodeMappingId, boolean isConversationOrigin, String processType)
-            throws NotFoundException {
-        IdentifierManager.registerMethodIdentifier("getPublicKey", "GPK");
-        logger.info(
-                "Iniciando el proceso de consultar la llave pública con los datos: {}, {}, {}",
-                nodeMappingId,
-                isConversationOrigin,
-                processType);
-        KeyResponseDTO keyresponse =
-                nodeMappingAsyncInputPort.getKeyAlias(
-                        nodeMappingId, isConversationOrigin, processType);
+    public ResponseEntity<KeyResponseDTO> getPublicKey(String nodeMappingId, boolean isConversationOrigin, String processType) throws NotFoundException {
+        IdentifierManager.registerMethodIdentifier("getPublicKey","GPK");
+        logger.info("Iniciando el proceso de consultar la llave pública con los datos: {}, {}, {}", nodeMappingId, isConversationOrigin, processType);
+        KeyResponseDTO keyresponse = nodeMappingAsyncInputPort.getKeyAlias(nodeMappingId, isConversationOrigin, processType);
         if (keyresponse == null) {
             return new ResponseEntity<>(new KeyResponseDTO(), HttpStatus.NOT_FOUND);
         }
