9c2ec54117b7f876f9ed9eb722067675c33a2296
Hiram Damian <hiramdamian99@gmail.com>
2024-11-13 19:24:37 -0600

    f0 Calidad: se eliminan modulos

--- domain/src/main/java/com/veradat/nodemanager/domain/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.veradat.nodemanager.domain.exception;
-
-import com.veradat.commons.exception.CustomValidationException;
-
-public class NotFoundException extends CustomValidationException {
-    public NotFoundException(String message, int exceptionId) {
-        super(message, exceptionId);
-    }
-}
